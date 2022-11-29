ea906af1f28ca13b9b137535840a16b96e4df6a6
liyajun <332209078@qq.com>
2022-11-29 18:48:48 +0800

    新增分页管理后台分页

--- src/components/Admin/Admin_Main.vue
+++ src/components/Admin/Admin_Main.vue
@@ -4,15 +4,15 @@
       <el-menu :default-openeds="['1','2', '3']">
         <el-submenu index="1">
           <template slot="title"><i class="el-icon-message"></i>用户管理</template>
-            <el-menu-item index="1-1">用户信息</el-menu-item>
+          <el-menu-item index="1-1">用户信息</el-menu-item>
         </el-submenu>
         <el-submenu index="2">
           <template slot="title"><i class="el-icon-menu"></i>订单管理</template>
-            <el-menu-item index="2-1">订单信息</el-menu-item>
+          <el-menu-item index="2-1">订单信息</el-menu-item>
         </el-submenu>
         <el-submenu index="3">
           <template slot="title"><i class="el-icon-setting"></i>商品管理</template>
-            <el-menu-item index="3-1">商品信息</el-menu-item>
+          <el-menu-item index="3-1">商品信息</el-menu-item>
         </el-submenu>
       </el-menu>
     </el-aside>
@@ -27,46 +27,97 @@
             <el-dropdown-item>删除</el-dropdown-item>
           </el-dropdown-menu>
         </el-dropdown>
-        <span>王小虎</span>
+        <span>{{ this.$store.getters.getAccount }}</span>
       </el-header>
+      <div style="margin: 10px 0">
+        <el-popconfirm
+            class="ml-5"
+            confirm-button-text='确定'
+            cancel-button-text='我再想想'
+            icon="el-icon-info"
+            icon-color="red"
+            title="您确定批量删除这些数据吗？"
+            @confirm="handleDeleteBatch">
+          <el-button type="danger" slot="reference">批量删除 <i class="el-icon-remove-outline" /></el-button>
+        </el-popconfirm>
+        <el-input style="width: 200px" placeholder="请输入用户名" suffix-icon="el-icon-search" v-model="searchAccount"></el-input>
+        <el-button class="ml-5" type="primary" @click="load">搜索</el-button>
+        <el-button type="warning" @click="reset">重置</el-button>
+
+      </div>
 
       <el-main>
-        <el-table :data="tableData">
-          <el-table-column prop="date" label="user_id" width="140"></el-table-column>
-          <el-table-column prop="name" label="user_name" width="120"></el-table-column>
-          <el-table-column prop="address" label="account"></el-table-column>
-          <el-table-column prop="address" label="phone"></el-table-column>
-          <el-table-column prop="address" label="createTime"></el-table-column>
-          <el-table-column prop="address" label="updateTime"></el-table-column>
-          <el-table-column prop="address" label="role"></el-table-column>
+        <el-table :data="tableData" @selection-change="handleSelectionChange">
+          <el-table-column type="selection" width="55"/>
+          <el-table-column prop="userId" label="用户id" width="140"></el-table-column>
+          <el-table-column prop="username" label="用户名" width="120"></el-table-column>
+          <el-table-column prop="account" label="用户账号"></el-table-column>
+          <el-table-column prop="phone" label="手机号"></el-table-column>
+          <el-table-column prop="createTime" label="创建时间"></el-table-column>
+          <el-table-column prop="updateTime" label="更新时间"></el-table-column>
+          <el-table-column prop="role" label="角色"></el-table-column>
+          <el-table-column label="操作" width="200" align="center">
+            <template slot-scope="scope">
+              <el-button type="success" @click="handleEdit(scope.row)">编辑 <i class="el-icon-edit"></i></el-button>
+              <el-popconfirm
+                  class="ml-5"
+                  confirm-button-text='确定'
+                  cancel-button-text='我再想想'
+                  icon="el-icon-info"
+                  icon-color="red"
+                  title="您确定删除吗？"
+                  @confirm="handleDelete(scope.row.userId)">
+                <el-button type="danger" slot="reference">删除 <i class="el-icon-remove-outline"></i></el-button>
+              </el-popconfirm>
+            </template>
+          </el-table-column>
         </el-table>
-        <pagination
-            :page="pageData.page"
-            :page-size="pageData.pageSize"
-            :total="total"
-            :on-page-change="onPage"
-            :showSizes="true"
-            :pageSizeList="pageData.pageSizeList"
-            :on-page-size-change="onSize"
-            class="pagi page-content"
-        />
+        <div style="padding: 10px 0">
+          <el-pagination
+              @size-change="handleSizeChange"
+              @current-change="handleCurrentChange"
+              :current-page="pageData.page"
+              :page-sizes="[ 5, 10, 20]"
+              :page-size="pageData.pageSize"
+              layout="total, sizes, prev, pager, next, jumper"
+              :total="pageData.pageTotal">
+          </el-pagination>
+        </div>
       </el-main>
     </el-container>
+    <el-dialog title="用户信息" :visible.sync="dialogFormVisible" width="30%">
+      <el-form label-width="80px" size="small">
+        <el-form-item label="用户名">
+          <el-input v-model="form.username" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="账号" >
+          <el-input v-model="form.account" autocomplete="off" :disabled="true"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱">
+          <el-input v-model="form.phone" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="电话">
+          <el-input v-model="form.phone" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="角色">
+          <el-input v-model="form.role" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="save">确 定</el-button>
+      </div>
+    </el-dialog>
   </el-container>
-
-
 
 
 </template>
 
 <script>
 import request from "@/utils/request";
-import Pagination from "@/components/Admin/Pagination.vue";
+
 export default {
   name: "Admin_Main",
-  components: {
-    Pagination,
-  },
   data() {
     // const item = {
     //   date: '2016-05-02',
@@ -74,21 +125,108 @@ export default {
     //   address: '上海市普陀区金沙江路 1518 弄'
     // };
     return {
-      tableData:[],
+      tableData: [],
       pageData: {
         pageTotal: 0,
         page: 1,
         pageSize: 10,
         pageSizeList: [10, 20, 30],
       },
+      form: {},
+      dialogFormVisible: false, //对话框
+      searchAccount:'',//模糊搜索
+      multipleSelection: [] //多选框
     }
   },
-  computed:{
+  computed: {
     total() {
       return this.pageData.pageTotal || 0;
     },
   },
   methods: {
+    handleSelectionChange(val){
+      this.multipleSelection = val
+    },
+    // 批量删除
+    handleDeleteBatch(){
+      let ids = this.multipleSelection.map(v => v.userId)//将对象数组转化为纯数组
+      console.log(this.multipleSelection)
+      console.log("输出ids")
+      console.log(ids)
+      this.request.delete("/userController/delUserByIds",{
+        data: ids
+      }).then(res => {
+        if(res.code == 200){
+          this.$notify.success(res.message)
+          this.load()
+        }else{
+          this.$notify.error("批量删除失败")
+        }
+      })
+    },
+    //重置
+    reset(){
+      this.searchAccount = ""
+      this.load()
+    },
+    //根据id删除
+    handleDelete(id) {
+      console.log(id)
+      this.request.delete('/userController/delUserById',{
+        params:{
+          id
+        }
+      }).then(res =>{
+        if (res.code == 200){
+          this.$notify.success("删除成功")
+          this.load()
+        }
+      })
+    },
+    //修改某一行
+    handleEdit(row) {
+      this.form = row;
+      this.dialogFormVisible = true;
+      console.log(row)
+    },
+    //保存更新
+    save() {
+        this.request.post('/userController/updateUser',{
+          "userId": this.form.userId,
+          "username": this.form.username ,
+          "phone": this.form.phone ,
+          "role": this.form.role ,
+        }).then(res =>{
+          if (res.code == 200){
+            this.$notify.success(res.message)
+            this.dialogFormVisible = false
+            this.load()
+          }
+        })
+    },
+    //刷新加载
+    load() {
+      this.request.get("/userController/getUserPage", {
+        params: {
+          pageNo: this.pageData.page,
+          pageSize: this.pageData.pageSize,
+          account: this.searchAccount,
+        }
+      }).then(res => {
+        this.tableData = res.data.records;
+        this.pageData.pageTotal = res.data.total;
+      })
+    },
+    //修改页面大小
+    handleSizeChange(pageSize) {
+      this.pageData.pageSize = pageSize
+      this.load()
+    },
+    //修改当前页数
+    handleCurrentChange(pageNum) {
+      this.pageData.page = pageNum
+      this.load()
+    },
     // 分页
     onPage(pageNow) {
       console.log('pageNow', pageNow);
@@ -100,14 +238,18 @@ export default {
   },
   mounted() {
     request
-        .get("userController/getUserPage",{
-          params:{
-            pageNo:1,
-            pageSize:5,
-            account:this.$store.getters.getAccount
+        .get("/userController/getUserPage", {
+          params: {
+            pageNo: 1,
+            pageSize: 5,
           }
         })
-        .then(res=>{
+        .then(res => {
+          this.tableData = res.data.records;
+          this.pageData.pageSize = 5;
+          this.pageData.page = 1;
+          this.pageData.pageTotal = res.data.total;
+          console.log(this.tableData)
           console.log(res);
         })
   }
